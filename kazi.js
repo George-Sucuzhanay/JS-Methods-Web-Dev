f71b7f5a534c7edcb46936e4ae4b11650dc1a07d
Kazi Anwar <kazisameen2702@gmail.com>
2023-09-22 20:53:04 -0400

    grabKeys works kind of?

--- kazi.js
+++ kazi.js
@@ -13,3 +13,22 @@ function myLastIndexOf(arr, targetElement)
     
     return -1; // Return -1 if the element is not found aka it's not in the array
 }
+
+// grabKeys function
+function grabKeys(obj) 
+{
+    return Object.keys(obj);
+}
+
+function runGrabKeys() 
+{
+    // Get the input object from user input
+    const inputObjectStr = document.getElementById('inputObject').value;
+    const inputObject = JSON.parse(inputObjectStr); // Parse the input as JSON to convert it to an object
+
+    // Call the grabKeys function
+    const keys = grabKeys(inputObject);
+
+    // Display the keys
+    document.getElementById('resultKeysDisplay').textContent = JSON.stringify(keys);
+}
