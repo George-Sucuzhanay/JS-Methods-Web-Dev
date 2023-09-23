fa89bb52758ac58228cce88c9d6007c0f54c254b
Kazi Anwar <kazisameen2702@gmail.com>
2023-09-22 21:37:12 -0400

    grabValues works!

--- kazi.js
+++ kazi.js
@@ -32,3 +32,22 @@ function runGrabKeys() //runs when button is clicked
     // Display the keys by converitng it back to a string
     document.getElementById('resultKeysDisplay').textContent = JSON.stringify(keys); //stringify converts keys back to string and then in the html file resultKeysDisplay its displayed 
 }
+
+// grabValues function
+function grabValues(obj) //defines function named grabValues that takes one parameter with parameter obj from which we wanna obtain the values from
+{
+    return Object.values(obj); //return an array containing all the values of the obj
+}
+
+function runGrabValues() //runs when button is clicked
+{
+    // Get the input object from user input
+    const inputObjectStr1 = document.getElementById('inputObject1').value; //gets string value stored in inputObject1 by id
+    const inputObject1 = JSON.parse(inputObjectStr1); // if the input is formatted properly, this parses it and makes an obj outta it stored in inputObject1
+
+    // Call the grabValues function i made earlier
+    const values = grabValues(inputObject1); //from the object, ur now retrieiving the values
+
+    // Display the values by converitng it back to a string
+    document.getElementById('resultValuesDisplay').textContent = JSON.stringify(values); //stringify converts values back to string and then in the html file resultValuesDisplay its displayed 
+}
