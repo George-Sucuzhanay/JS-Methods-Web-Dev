a0a354d254a9007c4ff6281d2c5daef850304aca
Rahatk1 <rahatkhandokar5@gmail.com>
2023-09-22 21:56:18 -0400

    myEach() methods works

---
+++ rahat.js
@@ -0,0 +1,28 @@
+function myEach(array, callback) {
+    for (let i = 0; i < array.length; i++) { // for loop that runs through the array 
+        callback(array[i], i, array); // for each iteration the call back function is iterated. 
+        console.log(array) // prints values of the array
+    }
+}
+
+function displayNumbers() {
+    const inputValue = document.getElementById('numberInput').value; // gets value of users input from the HTML file
+    const numbersArray = inputValue.split(',').map(num => num.trim()); //Convert Input String to an Array of Numbers
+
+    //This just clears the div 
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = ''; // Clear previous output
+
+
+// calls the myeach fucntion that iterates through the array 
+    myEach(numbersArray, function(num) {
+        //creates a pragroh elemetn that holds the values 
+        const paragraph = document.createElement('p');
+        // the values are gonna be stored as num
+        paragraph.innerText = num;
+        // appends the paragraph into the output div
+        outputDiv.appendChild(paragraph);
+    });
+}
+
+
