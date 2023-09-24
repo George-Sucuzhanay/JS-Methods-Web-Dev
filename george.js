35272be756ca5be92d5ea94a51aa4ade70233a81
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-09-23 22:27:21 -0400

    added myIncludes and myIndexOf JS funcs and removed some html boilerplate

---
+++ george.js
@@ -0,0 +1,22 @@
+//  myIncludes
+
+function myIncludes(arr, targetElement){
+    for(const i of arr){
+        if(i == targetElement){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// myIndexOf
+
+function myIndexOf(arr, targetElement){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == targetElement){
+            return i;
+        }
+    }
+    return -1;
+}
