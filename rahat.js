4a2595ebeb45f11bf8611ba043ec0106666818ee
Rahatk1 <rahatkhandokar5@gmail.com>
2023-09-23 23:13:28 -0400

    finished map method

--- rahat.js
+++ rahat.js
@@ -16,13 +16,40 @@ function displayNumbers() {
 
 // calls the myeach fucntion that iterates through the array 
     myEach(numbersArray, function(num) {
-        //creates a pragroh elemetn that holds the values 
-        const paragraph = document.createElement('p');
-        // the values are gonna be stored as num
-        paragraph.innerText = num;
-        // appends the paragraph into the output div
-        outputDiv.appendChild(paragraph);
+        outputDiv.innerHTML += `${num}<br>`;
     });
+
+
+
 }
 
 
+// Map allows you to run a function on each element in the array and returns a modified version
+
+function myMap(array, callback) {
+    let result = [];  // Initialize an empty array for the results
+    for (let i = 0; i < array.length; i++) { // loops thru array 
+        result.push(callback(array[i], i, array));  // Invoke the callback with the current element, its index, and the original array, then push its result into the "result" array.
+    }
+    return result;  // Return the newly constructed array after processing all elements
+}
+
+// created a function to read the valies that the user put and it spits it back into the html displaying it 
+function displayDoubledNumbers() { 
+    const inputValue = document.getElementById('mapInput').value; // gets the values from the div by id
+    const numbersArray = inputValue.split(',').map(num => num.trim()); // makes it clean 
+
+    // Use the myMap function to double the numbers
+    const doubledNumbers = myMap(numbersArray, function(num) { // new vals are stored here 
+        return num * 2; // multiply numbers by 2 
+    });
+
+    //This just clears the div 
+    const output2Div = document.getElementById('output2');
+    output2Div.innerHTML = ''; // Clear previous output
+
+    // Use myEach to display the doubled numbers REUSED CODE
+    myEach(doubledNumbers, function(num) {
+        output2Div.innerHTML += `${num}<br>`;
+    });
+}
